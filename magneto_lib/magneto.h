e660ba24345a6b8124e863ccaf27976c2b2bd71a
Araks <arakstigranyan8gmail.com>
2021-04-24 23:38:42 +0400

    Changing the namspace.

--- magneto_lib/magneto.h
+++ magneto_lib/magneto.h
@@ -3,10 +3,12 @@
 
 namespace cr {
 namespace magneto {
+namespace chessary {
 
 // Defines library API
 
 }  // namespace magneto
+}  // namespace chessary
 }  // namespace cr
 
 #endif // MAGNETO_LIB_MAGNETO_H
