b9b1f68370b7b68c8eb82dee3f0802d8102bd8a6
knyazyan-lilit <lilitknyazyan01@gmail.com>
2021-05-05 12:33:38 +0400

    Create Square and Piece structs

--- magneto_lib/magneto.cpp
+++ magneto_lib/magneto.cpp
@@ -1,1 +1,26 @@
 #include "magneto.h"
+#include <string>
+#include <map>
+
+ cr::chess::Square::Square(){
+        std::map <int, std::map<int,std::string>> map;
+        int number = 8;
+        for(int i = 0;i < 8; ++i ){
+            std::string letter = "a";
+            for (int j = 0; j < 8 ; ++j)
+            {
+                map[i][j] = letter + std::to_string(number);
+                char tmp=letter[0];
+                tmp++;
+                letter = std::string(1,tmp);
+            }
+            --number;
+        }
+    }
+
+ cr::chess::Square::operator std::string()const{
+
+        return  this->map.find(this->row)->second.find(this->column)->second;
+
+ }
+
--- magneto_lib/magneto.h
+++ magneto_lib/magneto.h
@@ -1,10 +1,38 @@
 #ifndef MAGNETO_LIB_MAGNETO_H
 #define MAGNETO_LIB_MAGNETO_H
-
+#include <map>
 namespace cr {
 namespace chess {
 
-// Defines library API
+    enum  TypeOfPiece {
+        king,          
+        queen,
+        rook,
+        pawn,       
+        bishop,    
+        knight,
+         
+    };
+
+    enum Color{
+        white,
+        black,
+    };
+
+    struct Square{
+        int row;
+        int column;
+        std::map <int, std::map<int,std::string>> map;
+        operator std::string() const;
+        Square();
+    };
+
+    struct Piece{
+        TypeOfPiece piece;
+        Color color;
+        Piece();
+    };	
+
 
 }  // namespace chess
 }  // namespace cr
